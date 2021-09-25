78ae4bbfdc7913e613f33487b1df44c34a22e110
M. Rehan Abbasi <mrehanabbasi@yahoo.com>
2021-09-25 06:29:59 +0500

    fix sign_into_meta function

--- openseaupload.py
+++ openseaupload.py
@@ -17,13 +17,14 @@ load_dotenv()
 
 testnet_url = 'https://testnets.opensea.io/asset/create'
 mainnet_url = 'https://opensea.io/asset/create'
+project_name = 'Arabic Calligraphy'
 
 
-def uploadFiles(start_item_id, count, is_rinkeby, metamask_exists):
+def upload_files(start_item_id, count, is_rinkeby):
     chop = webdriver.ChromeOptions()
-    if not metamask_exists:
-        chop.add_extension('MetaMask_v10.1.1.crx')
-    driver = webdriver.Opera(options=chop)
+    chop.add_extension('Metamask-10.1.1.crx')
+    driver = webdriver.Chrome(options=chop)  # Using Chrome browser
+    driver.maximize_window()
     wait = WebDriverWait(driver, 60)
     df = pandas.read_csv('Generated/metadata.csv')
     if is_rinkeby:
@@ -33,11 +34,9 @@ def uploadFiles(start_item_id, count, is_rinkeby, metamask_exists):
     driver.get(url)
     time.sleep(0.5)
 
-    # If Metamask extension is not already installed and not logged into OpenSea
-    if not metamask_exists:
-        passphrase = os.getenv('METAMASK_PASSPHRASE')
-        wallet_pwd = os.getenv('METAMASK_PASSWORD')
-        signIntoMeta(driver, wait, is_rinkeby, passphrase, wallet_pwd)
+    passphrase = os.getenv('METAMASK_PASSPHRASE')
+    wallet_pwd = os.getenv('METAMASK_PASSWORD')
+    sign_into_meta(driver, wait, is_rinkeby, passphrase, wallet_pwd)
 
     tabs2 = driver.window_handles
     driver.switch_to.window(tabs2[1])
@@ -54,88 +53,97 @@ def uploadFiles(start_item_id, count, is_rinkeby, metamask_exists):
             createPage = driver.find_element_by_xpath(
                 '//*[@id="__next"]/div[1]/div[1]/nav/ul/li[4]/a')
             createPage.click()
-        filePath = 'Generated\\{} ABCs {} {}.png'.format(
-            item_id, row['Letter Permutation'], row['Hat'])
-        print(filePath, item_id, row['Background'], row['Font'], row['Font & Colour Combination'],
+        # file_path = 'Generated\\{} ABCs {} {}.png'.format(
+        #     item_id, row['Letter Permutation'], row['Hat'])
+        file_path = f"Generated\{str(item_id)} {project_name} {row['Letter Permutation']} {row['Hat']}.png"
+        print(file_path, item_id, row['Background'], row['Font'], row['Font & Colour Combination'],
               row['Font Colour'], row['Hat'], row['Letter 1'], row['Letter 2'],
               row['Letter 3'], row['Letter Permutation'], row['Special'], row['Name'])
+
+        # Upload image
         wait.until(ExpectedConditions.presence_of_element_located(
             (By.XPATH, '//*[@id="media"]')))
-        imageUpload = driver.find_element_by_xpath('//*[@id="media"]')
-        imagePath = os.path.abspath(
-            filePath)
-        imageUpload.send_keys(imagePath)
+        image_upload = driver.find_element_by_xpath('//*[@id="media"]')
+        image_path = os.path.abspath(file_path)
+        image_upload.send_keys(image_path)
 
         name = driver.find_element_by_xpath('//*[@id="name"]')
         name.send_keys(row['Name'])
         description = driver.find_element_by_xpath('//*[@id="description"]')
         description.send_keys(row['Letter Permutation'])
-        collectionName = driver.find_element_by_xpath(
+        collection_name = driver.find_element_by_xpath(
             '//*[@id="__next"]/div[1]/main/div/div/section/div/form/section[5]/div/input')
-        collectionName.send_keys('The ABCs')
-        collectionButtonFromListName = '//button[normalize-space()="{}"]'.format(
-            'The ABCs')
+        collection_name.send_keys(project_name)
+        # collection_button_from_list_name = '//button[normalize-space()="{}"]'.format(
+        #     'The ABCs')
+        collection_button_from_list_name = f'//button[normalize-space()="{project_name}"]'
         wait.until(ExpectedConditions.presence_of_element_located(
-            (By.XPATH, collectionButtonFromListName)))
-        collectionButtonFromList = driver.find_element_by_xpath(
-            collectionButtonFromListName)
-        collectionButtonFromList.click()
+            (By.XPATH, collection_button_from_list_name)))
+        collection_button_from_list = driver.find_element_by_xpath(
+            collection_button_from_list_name)
+        collection_button_from_list.click()
         time.sleep(0.1)
 
-        propertiesPlusButton = driver.find_element_by_xpath(
+        properties_plus_button = driver.find_element_by_xpath(
             '//*[@id="__next"]/div[1]/main/div/div/section/div/form/section[6]/div[1]/div/div[2]/button')
-        propertiesPlusButton.click()
-        print('starting properties population')
+        properties_plus_button.click()
+        print('Starting properties population...')
         for i, (key, value) in enumerate(row.items()):
             if key in ['ID', 'Special', 'Name']:
                 continue
             wait.until(ExpectedConditions.presence_of_element_located(
                 (By.XPATH, '//button[normalize-space()="Add more"]')))
-            collectionAddPropButton = driver.find_element_by_xpath(
+            collection_add_prop_button = driver.find_element_by_xpath(
                 '//button[normalize-space()="Add more"]')
-            collectionAddPropButton.click()
-            propDivNum = 3
-            propKeyInputXpath = '/html/body/div[{}]/div/div/div/section/table/tbody/tr[{}]/td[1]/div/div/input'.format(
-                propDivNum, i + 1)
-            if len(driver.find_elements_by_xpath(propKeyInputXpath)) <= 0:
-                propDivNum = 2
-                propKeyInputXpath = '/html/body/div[{}]/div/div/div/section/table/tbody/tr[{}]/td[1]/div/div/input'.format(
-                    propDivNum, i + 1)
+            collection_add_prop_button.click()
+            prop_div_num = 3
+            # prop_key_input_xpath = '/html/body/div[{}]/div/div/div/section/table/tbody/tr[{}]/td[1]/div/div/input'.format(
+            #     prop_div_num, i + 1)
+            prop_key_input_xpath = f'/html/body/div[{prop_div_num}]/div/div/div/section/table/tbody/tr[{i+1}]/td[1]/div/div/input'
+            if len(driver.find_elements_by_xpath(prop_key_input_xpath)) <= 0:
+                prop_div_num = 2
+                # prop_key_input_xpath = '/html/body/div[{}]/div/div/div/section/table/tbody/tr[{}]/td[1]/div/div/input'.format(
+                #     prop_div_num, i + 1)
+                prop_key_input_xpath = f'/html/body/div[{prop_div_num}]/div/div/div/section/table/tbody/tr[{i+1}]/td[1]/div/div/input'
             # /html/body/div[2]/div/div/div/section/table/tbody/tr[2]/td[1]/div/div/input
             # /html/body/div[3]/div/div/div/section/table/tbody/tr[1]/td[1]/div/div/input
             time.sleep(0.1)
             wait.until(ExpectedConditions.presence_of_element_located(
-                (By.XPATH, propKeyInputXpath)))
-            propKey = driver.find_element_by_xpath(propKeyInputXpath)
-            propKey.send_keys(key)
-            propKey = driver.find_element_by_xpath(
-                '/html/body/div[{}]/div/div/div/section/table/tbody/tr[{}]/td[2]/div/div/input'.format(propDivNum,
-                                                                                                       i + 1))
-            propKey.send_keys(value)
-        propSave = driver.find_element_by_xpath(
-            '/html/body/div[{}]/div/div/div/footer/button'.format(propDivNum))
-        propSave.click()
-        print('completed properties population')
+                (By.XPATH, prop_key_input_xpath)))
+            prop_key = driver.find_element_by_xpath(prop_key_input_xpath)
+            prop_key.send_keys(key)
+            # prop_key = driver.find_element_by_xpath(
+            #     '/html/body/div[{}]/div/div/div/section/table/tbody/tr[{}]/td[2]/div/div/input'.format(prop_div_num,
+            #                                                                                            i + 1))
+            prop_key = driver.find_element_by_xpath(
+                f'/html/body/div[{prop_div_num}]/div/div/div/section/table/tbody/tr[{i+1}]/td[2]/div/div/input')
+            prop_key.send_keys(value)
+        # prop_save = driver.find_element_by_xpath(
+        #     '/html/body/div[{}]/div/div/div/footer/button'.format(prop_div_num))
+        prop_save = driver.find_element_by_xpath(
+            f'/html/body/div[{prop_div_num}]/div/div/div/footer/button')
+        prop_save.click()
+        print('Completed properties population')
         time.sleep(0.5)
-        createNFT = driver.find_element_by_xpath(
+        create_NFT = driver.find_element_by_xpath(
             '//*[@id="__next"]/div[1]/main/div/div/section/div/form/div/div[1]/span/button')
         # time.sleep(5000)
-        createNFT.click()
+        create_NFT.click()
         print('creating nft ', item_id, row['Background'], row['Font'], row['Font & Colour Combination'],
               row['Hat'], row['Letter Permutation'])
         '/html/body/div[4]/div/div/div/div[1]/header/h4'
         wait.until(ExpectedConditions.presence_of_element_located(
             (By.XPATH, "/html/body/div[4]/div/div/div/div[1]/header/h4")))
         try:
-            closeCreateModal = driver.find_element_by_xpath(
+            close_create_model = driver.find_element_by_xpath(
                 '/html/body/div[4]/div/div/div/div[2]/button/i')
-            closeCreateModal.click()
+            close_create_model.click()
         except:
-            print('Close Create Modal not found for ',
+            print('Close Create Model not found for ',
                   item_id, row['Letter Permutation'])
 
 
-def signIntoMeta(driver, wait, isrinkeby, mnemonicString, walletPwd):
+def sign_into_meta(driver, wait, is_rinkeby, passphrase, wallet_pwd):
     tabs2 = driver.window_handles
     driver.switch_to.window(tabs2[0])
     time.sleep(0.5)
@@ -153,56 +161,63 @@ def signIntoMeta(driver, wait, isrinkeby, mnemonicString, walletPwd):
         '//*[@id="app-content"]/div/div[3]/div/div/div/div[5]/div[1]/footer/button[1]')
     button.click()
     time.sleep(0.5)
-    mnemonicInput = driver.find_element_by_xpath(
-        '//*[@id="app-content"]/div/div[3]/div/div/form/div[4]/div[1]/div/input')
-    mnemonicInput.send_keys(mnemonicString)
-    pwd1Input = driver.find_element_by_xpath('//*[@id="password"]')
-    pwd1Input.send_keys(walletPwd)
-    pwd2Input = driver.find_element_by_xpath('//*[@id="confirm-password"]')
-    pwd2Input.send_keys(walletPwd)
+    passphrase_input = driver.find_element_by_xpath(
+        '//*[@id = "app-content"]/div/div[3]/div/div/form/div[4]/div[1]/div/input')
+    # //*[@id = "app-content"]/div/div[3]/div/div/form/div[4]/div[1]/div/input
+    passphrase_input.send_keys(passphrase)
+    pwd_1_input = driver.find_element_by_xpath('//*[@id="password"]')
+    pwd_1_input.send_keys(wallet_pwd)
+    pwd_2_input = driver.find_element_by_xpath('//*[@id="confirm-password"]')
+    pwd_2_input.send_keys(wallet_pwd)
     checkbox = driver.find_element_by_xpath(
         '//*[@id="app-content"]/div/div[3]/div/div/form/div[7]/div')
     checkbox.click()
     submit = driver.find_element_by_xpath(
         '//*[@id="app-content"]/div/div[3]/div/div/form/button')
     submit.click()
-    time.sleep(1)
+    time.sleep(3)
     alldone = driver.find_element_by_xpath(
         '//*[@id="app-content"]/div/div[3]/div/div/button')
     alldone.click()
     time.sleep(1)
-    xmodal = driver.find_element_by_xpath(
+    x_model = driver.find_element_by_xpath(
         '//*[@id="popover-content"]/div/div/section/header/div/button')
-    xmodal.click()
+    x_model.click()
     time.sleep(0.01)
-    if isrinkeby:
+    if is_rinkeby:
         network = driver.find_element_by_xpath(
             '//*[@id="app-content"]/div/div[1]/div/div[2]/div[1]/div')
         network.click()
         time.sleep(0.1)
-        networkRinkeby = driver.find_element_by_xpath(
+        network_rinkeby = driver.find_element_by_xpath(
             '//*[@id="app-content"]/div/div[3]/div/li[4]')
-        networkRinkeby.click()
+        network_rinkeby.click()
     driver.switch_to.window(tabs2[1])
     time.sleep(0.1)
-    oswalleticon = driver.find_element_by_xpath(
-        '//*[@id="__next"]/div[1]/div[1]/nav/ul/li[5]/button')
-    oswalleticon.click()
-    time.sleep(0.1)
-    metaicon = driver.find_element_by_xpath(
-        '//*[@id="__next"]/div[1]/aside/div[2]/div/div[2]/ul/li[1]/button')
-    metaicon.click()
-    time.sleep(1.5)
+    # os_wallet_icon = driver.find_element_by_xpath(
+    #     '//*[@id="__next"]/div[1]/div[1]/nav/ul/li[5]/button')
+    # # '//*[@id="__next"]/div[1]/main/div/div/div/div[1]/div[2]/button'
+    # os_wallet_icon.click()
+    # time.sleep(0.1)
+    # meta_icon = driver.find_element_by_xpath(
+    #     '//*[@id="__next"]/div[1]/aside/div[2]/div/div[2]/ul/li[1]/button')
+    # meta_icon.click()
+    signin_icon = driver.find_element_by_xpath(
+        '//*[@id="__next"]/div[1]/main/div/div/div/div[1]/div[2]/button')
+    signin_icon.click()
+    time.sleep(3.5)
     tabs2 = driver.window_handles
     driver.switch_to.window(tabs2[2])
-    connectnext = driver.find_element_by_xpath(
+    connect_next = driver.find_element_by_xpath(
         '//*[@id="app-content"]/div/div[3]/div/div[2]/div[4]/div[2]/button[2]')
-    connectnext.click()
+    connect_next.click()
     time.sleep(0.5)
     connect = driver.find_element_by_xpath(
         '//*[@id="app-content"]/div/div[3]/div/div[2]/div[2]/div[2]/footer/button[2]')
     connect.click()
-    time.sleep(0.5)
+    time.sleep(3.5)
+    tabs2 = driver.window_handles
+    driver.switch_to.window(tabs2[2])
     wait.until(ExpectedConditions.presence_of_element_located(
         (By.XPATH, '//*[@id="app-content"]/div/div[3]/div/div[3]/button[2]')))
     sign = driver.find_element_by_xpath(
@@ -211,5 +226,4 @@ def signIntoMeta(driver, wait, isrinkeby, mnemonicString, walletPwd):
 
 
 if __name__ == '__main__':
-    uploadFiles(start_item_id=186, count=65,
-                is_rinkeby=False, metamask_exists=True)
+    upload_files(start_item_id=1, count=65, is_rinkeby=False)
